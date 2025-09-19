376ea6ff42291ffca3ba6103f00e97fd44fe4fad
Shakila shaik <skshakilask67@gmail.com>
2025-09-19 17:49:25 +0530

    practiced fs module

---
+++ Nodejs/node-prep-2025/os-fs-http-events/fsModule.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+
+// fs.writeFile('shammu.txt',"Hey i am from file shammu.txt",(err)=>{
+//     console.log("completed writing a file ")
+
+// })
+
+// fs.readFile('shammu.txt', 'utf8',(err,data)=>{
+//     console.log("completed file reading", data)
+//     console.log("errors is ", err)
+// })
+
+const a = fs.writeFileSync("shammu.txt", "I am chaning text")
+
+
+const b = fs.readFileSync("shammu.txt", 'utf-8'
+)
+console.log(b)
