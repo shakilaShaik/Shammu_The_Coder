60c9345c1b1bffdbfefcef35ca9b6a25fc1cc0d9
shammu shaik <skshakilask67@gmail.com>
2025-11-14 11:37:32 +0530

    implemented offset pagination in express app

--- Nodejs/node-prep-2025/pagination/backend/src/controllers.js
+++ Nodejs/node-prep-2025/pagination/backend/src/controllers.js
@@ -1,14 +1,31 @@
-import { User } from "./Model.js";
-export  const getUsers= async()=>{
-    
-    try {
-        const userList=await User.find()
-        console.log("the user list is", userList);
-        return userList
-    } catch (error) {
-        console.log(error);
-        return error
+export function pagination(userModel) {
+    return async (req, res, next) => {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = parseInt(req.query.limit) || 5
+
+
+            const startIdx = (page - 1) * limit
+
+            const userList = await userModel
+                .find()
+                .skip(startIdx)
+                .limit(limit).sort({_id:1})
+                console.log("the user list is", userList);
+
+            res.result = {
+                page,
+                limit,
+                count: userList.length,
+                data: userList,
+            }
+
+            next()
+        } catch (error) {
+            console.log("error from pagination", error)
+            return res.status(500).json({ error: "Pagination error", details: error })
+        }
     }
-    
-     }
+}
+
     
