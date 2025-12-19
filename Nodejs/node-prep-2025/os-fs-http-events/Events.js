7b8b6bc0b73e0155369590fa38e7070dedf35372
shammu <skshakilask67@gmail.com>
2025-12-19 18:03:42 +0530

    created sample project on next js

--- Nodejs/node-prep-2025/os-fs-http-events/Events.js
+++ Nodejs/node-prep-2025/os-fs-http-events/Events.js
@@ -1,20 +1,20 @@
 const http = require('http')
-const eventEmmitter = require('events')
+// const eventEmmitter = require('events')
 
-const server = http.createServer((req, res) => {
-    res.end("created a server")
+// const server = http.createServer((req, res) => {
+//     res.end("created a server")
 
-})
-server.listen(3000, () => {
-    console.log("server is listening on port 3000")
-})
+// })
+// server.listen(3000, () => {
+//     console.log("server is listening on port 3000")
+// })
 
-emmitter= new eventEmmitter()
-emmitter.on('call',()=>{
-    console.log("event is executed")
+// emmitter= new eventEmmitter()
+// emmitter.on('call',()=>{
+//     console.log("event is executed")
 
-})
-emmitter.emit('call') // trigger the event 
+// })
+// emmitter.emit('call') // trigger the event 
 
 const fs = require('fs');
 
@@ -23,18 +23,18 @@ const stream = fs.createReadStream('shammu.txt', { encoding: 'utf8' });
 
 // Listen for data chunks
 stream.on('data', (chunk) => {
-  console.log('Received chunk:', chunk);
+    console.log('Received chunk:', chunk);
 });
 
 // Listen for end of file
 stream.on('end', () => {
-  console.log('No more data.');
+    console.log('No more data.');
 });
 
 
 
 // Watch a file for changes
-fs.watch('shammu_test.txt', (eventType, filename) => {
+fs.watch('shammu.txt', (eventType, filename) => {
     if (filename) {
         console.log(`File ${filename} changed, Event type: ${eventType}`);
     }
